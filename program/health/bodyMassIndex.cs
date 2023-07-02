7b8de755290aaa8f02aac5d6663e2b2c6e736821
Piero Sebastiani <77357494+psebastiani2021@users.noreply.github.com>
2023-07-01 19:30:15 -0500

    c#: messages according to BMI

--- program/health/bodyMassIndex.cs
+++ program/health/bodyMassIndex.cs
@@ -19,6 +19,22 @@ namespace bodyMassIndex
             Console.WriteLine("Your height: " + height + " m");
             Console.WriteLine("Your approximated BMI is: " + Math.Round(BMI, 2));
             Console.WriteLine("Your specific BMI is: " + BMI);
+            if (BMI >= 1 && BMI <= 18.5) {
+                Console.WriteLine("Your BMI is considered as underweight.");
+            }
+            else if (BMI >= 18.51 && BMI <= 24.9) {
+                Console.WriteLine("Great! Your BMI is on the healthy range.");
+            }
+            else if (BMI >= 25 && BMI <= 29.9) {
+                Console.WriteLine("Your BMI is considered as overweight.");
+            }
+            else if (BMI >= 30 && BMI <= 39.9) {
+                Console.WriteLine("Your BMI is considered as obese weight.");
+            }
+            else if (BMI >= 40) {
+                Console.WriteLine("Your BMI is considered as severe obese weight.");
+            }
         }
     }
 }
+
