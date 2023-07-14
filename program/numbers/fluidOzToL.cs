cba6160cedac133d87c6b73c311ee4f763d7421f
Piero Sebastiani <77357494+psebastiani2021@users.noreply.github.com>
2023-07-14 09:27:59 -0500

    Create fluidOzToL.cs

---
+++ program/numbers/fluidOzToL.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace fluidOuncesToLiters
+{
+	class applicationContent
+	{
+		static void Main(string[] args)
+		{
+			double fluidOunces = Convert.ToDouble(Console.ReadLine());
+            double liters = fluidOunces/33.814;
+            string plural = "are";
+            Console.WriteLine("Enter the amount of fluid ounces: " + fluidOunces);
+            Console.WriteLine(fluidOunces + " fluid ounces are equal to " + Math.Round(liters, 7) + " liters.");
+ 		}
+	}
+}
